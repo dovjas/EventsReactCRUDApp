21162056b91882b3fe571278a4f84e70ef6de963
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-07-07 15:45:31 +0300

    Storing input data in variables

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [title,setTitle] = useState('');
+  const [location,setLocation] = useState('');
+  const [date,setDate] = useState(new Date());
+  const [price,setPrice] = useState(0);
+
+  const check =()=>{
+    console.log(title + location + date + price)
+  }
+
   return (
     <div className='container'>
       <h2>Events data</h2>
       <div className='input-block'>
         <label>Event title:</label>
-        <input type='text'></input>
+        <input type='text' onChange={(e)=>setTitle(e.target.value)}></input>
         <label>Event location:</label>
-        <input type='text'></input>
+        <input type='text' onChange={(e)=>setLocation(e.target.value)}></input>
         <label>Event date:</label>
-        <input type='date'></input>
+        <input type='date' onChange={(e)=>setDate(e.target.value)}></input>
         <label>Event price:</label>
-        <input type='number'></input>
+        <input type='number' onChange={(e)=>setPrice(e.target.value)}></input>
       </div>
       <div className='buttons'>
-        <button type='submit' className='add-btn'>Add Event</button>
+        <button type='submit' onClick={check} className='add-btn'>Add Event</button>
       </div>
     </div>
   );
