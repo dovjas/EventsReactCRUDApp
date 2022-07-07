6402d277f6b57db1987f8947c3f2cef9939ad8f3
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-07-07 15:26:43 +0300

    Data input fields

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <div></div>
+    <div className='container'>
+      <h2>Events data</h2>
+      <div className='input-block'>
+        <label>Event title:</label>
+        <input type='text'></input>
+        <label>Event location:</label>
+        <input type='text'></input>
+        <label>Event date:</label>
+        <input type='date'></input>
+        <label>Event price:</label>
+        <input type='number'></input>
+      </div>
+      <div className='buttons'>
+        <button type='submit' className='add-btn'>Add Event</button>
+      </div>
+    </div>
   );
 }
 
